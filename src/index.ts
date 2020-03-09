288f7fdf0ca55465999d874454ad076d99f6fe1d
Kevin Simper <kevin.simper@gmail.com>
2020-03-09 18:05:58 +0100

    Add basic graphql server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!"
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
+
+server.applyMiddleware({ app });
 
 app.get("/", (req, res) => {
   res.send("Unity Issue");
