e18003fa66fd241b684cc8de5ab319274afd328b
Kevin Simper <kevin.simper@gmail.com>
2020-03-09 21:38:04 +0100

    Add simple graphql backend call

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,3 +1,17 @@
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+
+const QUERY = gql`
+  {
+    hello
+  }
+`;
+
 export default () => {
-  return <div>Hello</div>;
+  const { loading, data } = useQuery(QUERY);
+
+  if (loading || !data) {
+    return <h1>loading...</h1>;
+  }
+  return <h1>{data.hello}</h1>;
 };
