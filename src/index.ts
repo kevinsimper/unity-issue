adb96724105dd4754dfe6b569a9d70cfa9b07b26
Kevin Simper <kevin.simper@gmail.com>
2020-03-09 17:56:32 +0100

    Add basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Unity Issue");
+});
+
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, () => console.log("Listening on localhost:" + PORT));
