68c48451f6bdb3d89c0a254708ac231caffee551
Kevin Simper <kevin.simper@gmail.com>
2020-03-09 20:34:54 +0100

    Use bcrypt to hash password

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,10 +1,16 @@
 import { setupDatabase } from "./db";
+import bcrypt from "bcrypt";
 
 const knex = setupDatabase();
 const table = "users";
 
 export async function createUser(input) {
   const db = await knex;
+  const password = await bcrypt.hash(input.password, 10);
+  const data = {
+    email: input.email,
+    password
+  };
   const rows = await db(table).insert(input);
   return rows;
 }
